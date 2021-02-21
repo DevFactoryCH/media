ff889c54bacad22b396f38217cb98aa658b06b4f
Mark Cameron <mark.cameron@devfactory.ch>
2021-02-21 18:29:36 +0100

    Use Illuminate\Support\Facades\Storage for Model url attribute

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -2,37 +2,42 @@
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
-class Media extends Model {
+class Media extends Model
+{
 
-  protected $table = 'media';
+    protected $table = 'media';
 
-  protected $fillable = [
-    'filename',
-    'mime',
-    'size',
-    'name',
-    'alt',
-    'title',
-    'group',
-    'status',
-    'weight',
-  ];
+    protected $fillable = [
+        'filename',
+        'mime',
+        'size',
+        'name',
+        'alt',
+        'title',
+        'group',
+        'status',
+        'weight',
+    ];
 
-  public function mediable() {
-    return $this->morphTo();
-  }
-
-  public function getUrlAttribute() {
-    return Url::asset(config('media.config.files_directory') . $this->filename);
-  }
-
-  public function getTitleAttribute($value) {
-    if (empty($value)) {
-      return basename($this->attributes['filename']);
+    public function mediable()
+    {
+        return $this->morphTo();
     }
 
-    return $value;
-  }
+    public function getUrlAttribute()
+    {
+        return Storage::url($this->filename);
+        //return Url::asset(config('media.config.files_directory') . $this->filename);
+    }
 
+    public function getTitleAttribute($value)
+    {
+        if (empty($value)) {
+            return basename($this->attributes['filename']);
+        }
+
+        return $value;
+    }
 }
